5436cd5e6d1287f8a1e4e5768f5398716175967a
dkokonkwo <d.okonkwo@alustudent.com>
2024-03-12 19:08:18 -0300

    database

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -5,7 +7,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+#  postgres://fee_generator_user:xV3iag0I4DddsLHsfh78f7dtQOTuAZEr@dpg-cnochuud3nmc739f3iog-a.oregon-postgres.render.com/fee_generator
+#  "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
